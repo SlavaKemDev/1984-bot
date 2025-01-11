010eae2a0aad619dbd1c32085acc487d6b4785be
Вячеслав Чертан <slavakemdev@gmail.com>
2025-01-11 23:03:25 +0300

    Update receive channel posts system. Update /ban command (now write right in channel posts)

--- TelegramBot/bot.py
+++ TelegramBot/bot.py
@@ -25,7 +25,8 @@ else:
     text_manager = TextManager(5, 0.7, timedelta(minutes=5))
 
 bot = AsyncTeleBot(os.environ['BOT_TOKEN'], parse_mode='HTML')
-CHANNEL_ID = os.environ['CHANNEL_ID']
+CHANNEL_ID = int(os.environ['CHANNEL_ID'])
+ADMIN_ID = int(os.environ['ADMIN_ID'])
 
 with open(f'{script_path}/config.json', 'r') as f:
     config = json.load(f)
@@ -78,16 +79,22 @@ def get_media_group_id(message: telebot.types.Message):
     return message.media_group_id or f"single_{message.message_id}"
 
 
-@bot.message_handler(content_types=['dice'])
+@bot.channel_post_handler(content_types=['dice'])
 async def remove_dice(message: telebot.types.Message):  # Remove all dices, except casino jackpot
+    if message.chat.id != CHANNEL_ID:
+        return
+
     is_jackpot = message.dice.emoji == '🎰' and message.dice.value in [1, 22, 43, 64]
 
     if REMOVE_DICE and (not is_jackpot or REMOVE_JACKPOT):
-        await bot.delete_message(CHANNEL_ID, message.forward_from_message_id)
+        await bot.delete_message(message.chat.id, message.message_id)
 
 
-@bot.message_handler(content_types=MODERATING_TYPES)
+@bot.channel_post_handler(content_types=MODERATING_TYPES)
 async def handle_post(message: telebot.types.Message):  # Handle all media messages
+    if message.chat.id != CHANNEL_ID:
+        return
+
     media_group_id = get_media_group_id(message)
     attachment = await parse_attachment(message)
 
@@ -115,7 +122,7 @@ async def handle_post(message: telebot.types.Message):  # Handle all media messa
         item = AttachmentItem(
             attachment_id=attachment.id,
             media_group_id=media_group_id,
-            message_id=message.forward_from_message_id,
+            message_id=message.message_id,
             created_at=datetime.now()
         )
         session.add(item)
@@ -123,25 +130,17 @@ async def handle_post(message: telebot.types.Message):  # Handle all media messa
         if not attachment.is_banned:
             session.commit()
         else:
-            await bot.delete_message(CHANNEL_ID, message.forward_from_message_id)
+            await bot.delete_message(CHANNEL_ID, message.message_id)
             session.rollback()
 
 
-@bot.message_handler(commands=['ban'])
+@bot.channel_post_handler(commands=['ban'])
 async def ban_content(message: telebot.types.Message):
-    if message.sender_chat and message.sender_chat.id == message.chat.id:
-        is_admin = True  # if sent by anonymous admin
-    else:
-        chat_id = message.chat.id
-        user_id = message.from_user.id
-
-        # Retrieve the user's status in the chat
-        chat_member = await bot.get_chat_member(chat_id, user_id)
-
-        # Check if the user is an administrator or the chat creator
-        is_admin = chat_member.status in ['administrator', 'creator']
-
-    if not is_admin:  # Command only for admins
+    if (message.chat.id != CHANNEL_ID
+            or not message.reply_to_message
+            or not message.from_user
+            or message.from_user.id != ADMIN_ID):
+        await bot.delete_message(message.chat.id, message.message_id)
         return
 
     target_message = message.reply_to_message
@@ -150,7 +149,8 @@ async def ban_content(message: telebot.types.Message):
     with Session() as session:
         attachment_items = session.query(AttachmentItem).filter_by(media_group_id=media_group_id).all()
 
-        args = list(map(int, message.text.split()[1:])) or list(range(1, len(attachment_items) + 1))  # args from the command
+        args = list(map(int, message.text.split()[1:])) or list(
+            range(1, len(attachment_items) + 1))  # args from the command
 
         for i in map(lambda x: x - 1, args):
             if i < 0 or i >= len(attachment_items):
@@ -170,22 +170,24 @@ async def ban_content(message: telebot.types.Message):
         session.commit()
 
 
-@bot.message_handler(commands=['mute'])
+@bot.channel_post_handler(commands=['mute'])
 async def mute(message: telebot.types.Message):
-    if not message.reply_to_message:
-        await bot.send_message(message.chat.id, "Эту команду нужно отправить в ответ на сообщение!")
+    if (message.chat.id != CHANNEL_ID
+            or not message.reply_to_message
+            or not message.from_user
+            or message.from_user.id != ADMIN_ID):
+        await bot.delete_message(message.chat.id, message.message_id)
         return
 
     text_manager.mark_explicit(message.reply_to_message.text.lower())
 
 
-@bot.message_handler(content_types=['text'])
+@bot.channel_post_handler(content_types=['text'])
 async def text_handler(message: telebot.types.Message):
     text = message.text.lower()
-    print(text)
 
     if not text_manager.check_is_available(text, datetime.now()):
-        await bot.delete_message(CHANNEL_ID, message.forward_from_message_id)
+        await bot.delete_message(CHANNEL_ID, message.message_id)
 
     text_manager.add_text(message.text.lower(), datetime.now())
 
@@ -195,5 +197,6 @@ async def main():
     loop.set_debug(True)
 
     await bot.polling(non_stop=True, request_timeout=60)
+
 
 asyncio.run(main())
